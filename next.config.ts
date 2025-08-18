0a27d4bfc8a2b9833373cbc387781f5ee729ba18
Le Quan <lvquanuit@gmail.com>
2025-08-18 13:53:05 +0700

    fix config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   basePath: '', // Replace with your repository name (e.g., '/wealthfarming-app')
   reactStrictMode: true, // Enable strict mode for development
   trailingSlash: true, // Ensure URLs end with a slash for GitHub Pages compatibility
+  images: {
+    unoptimized: true, // Disable Image Optimization for static export
+  },
 };
 
 export default nextConfig;
